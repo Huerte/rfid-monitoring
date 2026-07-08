58fdcfe0611bb54bb26a21f40b185c82dc9396da
Huerte <huertejerald@gmail.com>
2026-07-08 10:41:53 +0800

    chore: remove dead codes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\RfidScanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('/rfid-scans',  [RfidScanController::class, 'index']);
